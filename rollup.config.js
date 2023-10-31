252f39a08a424cf78e5ad292d058f0cb1f91b480
Desara Xhikola <xhikolad@gmail.com>
2023-10-31 13:39:54 +0100

    File configured

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+
+export default {
+  input: 'src/main.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'iife',
+  },
+  plugins: [resolve(), commonjs()],
+};
+
